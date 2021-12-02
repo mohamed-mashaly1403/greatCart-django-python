66759955e8adb1ed2fef688f080ac2719337ff89
mesho <14031981@Moh>
2021-12-03 03:06:38 +0400

    02-12-2021-local coding done-deploy-postgard

--- Cart/Cart/views.py
+++ Cart/Cart/views.py
@@ -6,6 +6,7 @@ from store.models import product,RatingReview
 
 def home(request):
     products = product.objects.all().filter( product_is_available=True).order_by('-product_created_date')
+    reviews =None
     for Product in products:
         reviews = RatingReview.objects.filter(product_id=Product.id,status=True)
 
