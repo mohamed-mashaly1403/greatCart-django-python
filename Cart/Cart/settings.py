ea00bbee2d29135e63428287f9f6b7ae254be33d
mesho <14031981@Moh>
2021-12-03 02:59:19 +0400

    02-12-2021-local coding done-deploy-postgard

--- Cart/Cart/settings.py
+++ Cart/Cart/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['django-cart-env.eba-mqmqviak.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-cart-env.eba-mqmqviak.us-west-2.elasticbeanstalk.com','127.0.0.1']
 
 
 # Application definition
@@ -89,12 +90,27 @@ AUTH_USER_MODEL = 'accounts.account'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES ={
+        'default':{
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'User':os.environ['RDS_USERNAME'],
+            'PASSWORD':os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
