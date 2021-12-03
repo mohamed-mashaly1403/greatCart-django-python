dab4f39d8e034e010d6be04b33d2d60fa9719f63
mesho <14031981@Moh>
2021-12-03 04:18:01 +0400

    03-12-2020

--- Cart/Cart/settings.py
+++ Cart/Cart/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['django-cart-env.eba-mqmqviak.us-west-2.elasticbeanstalk.com','127.0.0.1']
+ALLOWED_HOSTS = ['django-cart-env.eba-mqmqviak.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
